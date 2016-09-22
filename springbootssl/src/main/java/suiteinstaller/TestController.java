2d224c10e37c9e07145a460e5632e8aac3667b44
Chen, Heng (Sylar,XS-SH) heng.chen@hp.com <heng.chen@hp.com>
2016-09-22 16:43:27 +0800

    add spring boot ssl

---
+++ springbootssl/src/main/java/suiteinstaller/TestController.java
@@ -0,0 +1,23 @@
+package suiteinstaller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	@ResponseBody
+    public String installation() {
+		return "hello world";
+	}
+	
+	//this can handle a redirect
+	@RequestMapping(value="/", method=RequestMethod.GET)
+    public String index(Model model) {
+		return "redirect:Enable%20HTTPS%20in%20Spring%20Boot.html";
+	}
+}
