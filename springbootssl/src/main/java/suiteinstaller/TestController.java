9be9763cdb274260f83ecda24bd0883dd07a2660
Sylar Chen <chenheng415@163.com>
2016-09-25 11:44:34 +0800

    m

--- springbootssl/src/main/java/suiteinstaller/TestController.java
+++ springbootssl/src/main/java/suiteinstaller/TestController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-	
+
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	@ResponseBody
     public String installation() {
 		return "hello world";
 	}
-	
+
 	//this can handle a redirect
 	@RequestMapping(value="/", method=RequestMethod.GET)
     public String index(Model model) {
