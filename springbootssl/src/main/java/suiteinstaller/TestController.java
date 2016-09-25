e33126444b03565b960adef1e69520070ca95d71
Sylar Chen <chenheng415@163.com>
2016-09-25 12:35:00 +0800

    add spring boot log configuration

--- springbootssl/src/main/java/suiteinstaller/TestController.java
+++ springbootssl/src/main/java/suiteinstaller/TestController.java
@@ -1,5 +1,7 @@
 package suiteinstaller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	@ResponseBody
     public String installation() {
+        logger.debug("This is a debug message");
+        logger.info("This is an info message");
+        logger.warn("This is a warn message");
+        logger.error("This is an error message");
 		return "hello world";
 	}
 
@@ -19,5 +27,10 @@ public class TestController {
 	@RequestMapping(value="/", method=RequestMethod.GET)
     public String index(Model model) {
 		return "redirect:Enable%20HTTPS%20in%20Spring%20Boot.html";
+	}
+
+	@RequestMapping(value="/log", method=RequestMethod.GET)
+	public String loghtml(Model model) {
+		return "redirect:Using%20Logback%20with%20Spring%20Boot%20-%20Spring%20Framework%20Guru.html";
 	}
 }
